a8500b8c18db61993a64f296cc98f68e12c5bba0
zdszero <1335908147@qq.com>
2021-11-11 22:32:49 +0800

    Add main test for lru

--- test/main.cc
+++ test/main.cc
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <iostream>
+#include <time.h>
 
 #include "BufferPoolManager.h"
 
@@ -8,6 +9,7 @@ void PrintHelp(char *program_name) {
          "Usage:\n"
          "\t%s [options] [filename]\n"
          "Options\n"
+         "\t-h, --help\t\tdisplay help\n"
          "\t-l, --lru\t\tuse lru replacer\n"
          "\t-c, --clock\t\tuse clock replacer\n",
          program_name);
@@ -15,28 +17,36 @@ void PrintHelp(char *program_name) {
 
 int ParseOpt(char *opt) {
   size_t len = strlen(opt);
-  if (len == 2 && opt[0] == '_') {
+  if (len == 2 && opt[0] == '-') {
     return opt[1];
   }
   if (len == 5 && strcmp(opt + 2, "lru") == 0)
     return 'l';
+  if (len == 6 && strcmp(opt + 2, "help") == 0)
+    return 'h';
   if (len == 7 && strcmp(opt + 2, "clock") == 0)
     return 'c';
   return -1;
 }
 
 int main(int argc, char *argv[]) {
+  clock_t start_time = clock();
   if (argc != 3) {
     PrintHelp(argv[0]);
     exit(1);
   }
   int opt = ParseOpt(argv[1]);
-  int policy;
+  int policy = ReplacePolicy::Invalid;
   char *filename;
   if (opt == 'l') {
     policy = ReplacePolicy::LruReplacer;
+    printf("using lru replacement policy:\n");
   } else if (opt == 'c') {
+    printf("using clock replacement policy:\n");
     policy = ReplacePolicy::ClockReplacer;
+  } else if (opt == 'h') {
+    PrintHelp(argv[0]);
+    return 0;
   } else {
     fprintf(stderr, "Unknown option: %s.\n", argv[1]);
     exit(1);
@@ -45,7 +55,6 @@ int main(int argc, char *argv[]) {
   FILE *db_file = fopen("test.dbf", "r");
   /* create db with certain size */
   if (db_file == NULL) {
-    fclose(db_file);
     db_file = fopen("test.dbf", "w");
     void *buffer = malloc(50000 * PAGE_SIZE);
     fwrite(buffer, PAGE_SIZE, 50000, db_file);
@@ -54,15 +63,22 @@ int main(int argc, char *argv[]) {
   fclose(db_file);
   /* create buffer pool manager */
   BMgr *bmgr = new BMgr("test.db");
-  /* read data file */ 
+  /* read data file */
   FILE *data_file = fopen(filename, "r");
   if (data_file == NULL) {
     fprintf(stderr, "Error: file %s doesn't exist\n", filename);
     exit(1);
   }
-  int act, page_id;
-  while (fscanf(data_file, "%d,%d", &act, &page_id)) {
+  int is_dirty, page_id;
+  while (fscanf(data_file, "%d,%d", &is_dirty, &page_id) != EOF) {
+    bmgr->FixPage(page_id, is_dirty);
+    bmgr->UnfixPage(page_id);
   }
+  printf("hit number: %d\n", bmgr->GetHitNum());
+  printf("hit rate: %.2lf%%\n", bmgr->GetHitNum() * 100.0 / 500000);
+  printf("io number: %d\n", bmgr->GetIONum());
+  printf("time taken: %.2fs\n",
+         (double)(clock() - start_time) / CLOCKS_PER_SEC);
   fclose(data_file);
   return 0;
 }
